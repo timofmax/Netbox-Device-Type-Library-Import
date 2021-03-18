dffd818f693ab874d3c2d1e89c7c3e2bc9004a02
Timofey Privalov <timofmax@gmail.com>
2021-03-18 23:58:39 +0100

    Update settings.py
    fixing ENV

--- settings.py
+++ settings.py
@@ -12,4 +12,11 @@ MANDATORY_ENV_VARS = ["REPO_URL", "NETBOX_URL", "NETBOX_TOKEN"]
 
 for var in MANDATORY_ENV_VARS:
     if var not in os.environ:
-        raise EnvironmentError("Failed because {} is not set.".format(var))
+        REPO_URL = str(os.getenv("REPO_URL")) if str(os.getenv("REPO_URL")) != None else input(
+                                                                                        "Please provide source repo: ")
+        NETBOX_URL = str(os.getenv("NETBOX_URL")) if str(os.getenv("NETBOX_URL")) != None else input(
+                                                                                        "Please provide NETBOX_URL: ")
+        NETBOX_TOKEN = str(os.getenv("NETBOX_TOKEN")) if str(os.getenv("NETBOX_TOKEN")) != None else input(
+                                                                                        "Please provide NETBOX_TOKEN: ")
+        
+
