a620b03aaa42627ba319c7a8f6336cb62c43f077
Timofey Privalov <timofmax@gmail.com>
2021-03-18 23:39:58 +0100

    Update settings.py
    Added check for ENV
    cause didn't worked for docker, aws ubuntu

--- settings.py
+++ settings.py
@@ -2,9 +2,11 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-REPO_URL = str(os.getenv("REPO_URL"))
-NETBOX_URL = str(os.getenv("NETBOX_URL"))
-NETBOX_TOKEN = str(os.getenv("NETBOX_TOKEN"))
+REPO_URL = str(os.getenv("REPO_URL")) if str(os.getenv("REPO_URL")) != None else input("Please provide source repo: ")
+NETBOX_URL = str(os.getenv("NETBOX_URL")) if str(os.getenv("NETBOX_URL")) != None else input(
+    "Please provide NETBOX_URL: ")
+NETBOX_TOKEN = str(os.getenv("NETBOX_TOKEN")) if str(os.getenv("NETBOX_TOKEN")) != None else input(
+    "Please provide NETBOX_TOKEN: ")
 
 MANDATORY_ENV_VARS = ["REPO_URL", "NETBOX_URL", "NETBOX_TOKEN"]
 
